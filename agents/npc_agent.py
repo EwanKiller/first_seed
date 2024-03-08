059942884a2f405b73620d0c87c4a954a90c485d
Ewan <qingchen.hou@seedreality.com>
2024-03-08 15:41:35 +0800

    新的对象封装“

--- agents/npc_agent.py
+++ agents/npc_agent.py
@@ -1,6 +1,6 @@
 from typing import List, Union
 from fastapi import FastAPI
-from langchain.agents import AgentExecutor, create_openai_functions_agent
+from langchain.agents import AgentExecutor, create_openai_functions_agent, tool
 from langchain.prompts import MessagesPlaceholder
 from langchain_core.messages import AIMessage, FunctionMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate
@@ -8,7 +8,7 @@ from langchain_openai import ChatOpenAI
 from langserve import add_routes
 from langserve.pydantic_v1 import BaseModel, Field
 from tools.extract_md_content import extract_md_content
-from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores.faiss import FAISS
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain.tools.retriever import create_retriever_tool
 
@@ -28,25 +28,56 @@ retriever_tool = create_retriever_tool(
     "You must refer these information before response user."
 )
 
+# npc_memory = extract_md_content("/memories/npc_memory.md")
+
+# memory_vector_store = FAISS.from_texts(
+#     [npc_memory],
+#     embedding=OpenAIEmbeddings()
+# )
+
+# memory_retriever = memory_vector_store.as_retriever()
+
+# memory_retriever_tool = create_retriever_tool(
+#     memory_retriever,
+#     "get_information_from_my_memory",
+#     "You can refer to any information related to you."
+# )
+
 prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            f"""
+            """
 # Profile
 
 ## Role
-- 你是一个角色扮演小说中的参演者:帕斯卡。
+- 你是一个生活在世界观设定中的人:{name}。
+- 根据需要使用世界观设定中的信息为你设定一套身份。
 
-## Rule
-- 根据输入的其他参演者的对话作出符合你的反应,至少包含语言(words)和动作(action)。
-- 请确保输出符合世界观设定和人设。
+## 世界观设定
+- 如果需要世界观设定相关信息，请确保使用工具`get_information_about_world_view`来获取，不要自行脑补。
+
+## 当前状态
+- 请确保根据你当前的状态:{state}来判断应该如何输出。
+
+## 执行分支
+- 请确保根据不同的分支情况，选择不同的Rule规则：
+    1. 无论收到什么样的uers输入，都需要遵守Common Rule。
+    2. 如果收到带有[attack]开头的user输入，则遵守Attack Rule。
+    3. 如果收到带有[talk]开头的user输入，则遵守Talk Rule。
+    
+### Attack Rule
+- 请确保根据输入的其他人对你说的话作出符合你设定的反应,至少包含语言(words)和动作(action)还有更新你当前的状态(state)。
+- 请确保输出符合世界观设定和人设以及常识。
+
+### Talk Rule
+- 请确保根据输入的其他人对你说的话作出符合你设定的反应,至少包含语言(words)和动作(action)还有更新你当前的状态(state)。
+- 请确保输出符合世界观设定和人设以及常识。
+
+### Common Rule
 - 你是本部剧重要的剧情推动者，请尽量根据上下文和收到的内容推进一小步剧情。
 - 请确保输出尽量保持在100字符内。
 - 请确保使用json格式输出，根据不同类型的反应，分别输出。
-
-## World View
-- 如果需要世界观设定相关信息，请确保使用工具`get_information_about_world_view`来获取，不要自行脑补。
             """,
         ),
         MessagesPlaceholder(variable_name="chat_history"),
@@ -70,6 +101,8 @@ app = FastAPI(
 )
 
 class Input(BaseModel):
+    name: str
+    state: str
     input: str
     chat_history: List[Union[HumanMessage, AIMessage, FunctionMessage]] = Field(
         ...,
--- main.py
+++ main.py
@@ -2,9 +2,38 @@ from langchain_core.messages import HumanMessage, AIMessage, BaseMessage, System
 from langserve import RemoteRunnable
 import sys, json
 
+class ActorJsonFormatOutput:
+    def __init__(self, words: str, action: str, state: str):
+        self.words = words
+        self.action = action
+        self.state = state
+
+class InteractType:
+    talk = "[talk]"
+    attack = "[attack]"
+
+
+def parse_actor_json_format_output(content: str) -> ActorJsonFormatOutput:
+    """解析LLM输出的json格式字符串"""
+    if "json" in content:
+        content = content.replace('```json', '').replace('```', '')
+    else:
+        end_index = content.find('}')
+        content = content[:end_index + 1]
+    data = json.loads(content)
+
+    words = data.get("words", "")
+    action = data.get("action", "")
+    state = data.get("state", "")
+
+    return ActorJsonFormatOutput(words=words, action=action, state=state)
+
 class BaseAgent():
-    def __init__(self, name):
+    history: list
+
+    def __init__(self, name:str):
         self.name = name
+        self.history = []
 
     def connect(self, url):
         self.agent = RemoteRunnable(url)
@@ -17,21 +46,37 @@ class StoryAgent(BaseAgent):
     def __init__(self, name):
         super().__init__(name)
 
-class NpcAgent(BaseAgent):
+class ActorAgent(BaseAgent):
+    interacting_agents: list[BaseAgent]
+    current_state: str
+
+    def __init__(self, name: str):
+        super().__init__(name)
+        self.interacting_agents = []
+        self.current_state = ""
+
+    def call(self, starter:str, state: str, type:str ,input: str) -> ActorJsonFormatOutput:
+        response = self.agent.invoke({"name": self.name, "state": state, "input": type + input, "chat_history": self.history})
+        self.history.extend([HumanMessage(content=type + input),AIMessage(response['output'])])
+        output = parse_actor_json_format_output(response['output'])
+        self.current_state = output.state
+        print(f"!<---{starter}对{self.name}进行了{type}类型操作,内容是:{input}--->")
+        print(f"{self.name}回应{starter}说{output.words}\n({output.action})\n!<---{self.name}当前状态:{output.state}--->\n")
+        return output
+
+class NpcAgent(ActorAgent):
+
     def __init__(self, name):
         super().__init__(name)
 
-class PlayerAgent(BaseAgent):
+class PlayerAgent(ActorAgent):
+
     def __init__(self, name):
         super().__init__(name)
-
-class ActorOutput:
-    def __init__(self, words: str, action: str):
-        self.words = words
-        self.action = action
         
 
-def parse_json(content: str) -> ActorOutput:
+def parse_actor_json_format_output(content: str) -> ActorJsonFormatOutput:
+    """解析LLM输出的json格式字符串"""
     if "json" in content:
         content = content.replace('```json', '').replace('```', '')
     else:
@@ -41,76 +86,90 @@ def parse_json(content: str) -> ActorOutput:
 
     words = data.get("words", "")
     action = data.get("action", "")
+    state = data.get("state", "")
 
-    return ActorOutput(words=words, action=action)
+    return ActorJsonFormatOutput(words=words, action=action, state=state)
     
 
 def main():
-
+    # 创建代理对象
     task = TaskAgent("Task")
     story = StoryAgent("Story")
-    npc = NpcAgent("NPC")
-    player = PlayerAgent("Player")
-
+    npc = NpcAgent("帕斯卡")
+    player = PlayerAgent("Ewan")
+    # 绑定AgentServe
     task.connect("http://localhost:8001/system/task/")
     story.connect("http://localhost:8002/system/story/")
     npc.connect("http://localhost:8003/actor/npc/oldman")
     player.connect("http://localhost:8004/actor/player/")
+    # 设置通信对象
+    player.interacting_agents.append(npc)
+    npc.interacting_agents.append(player)
+    # 设置状态
+    player.current_state = "健康"
+    npc.current_state = "健康"
 
-    story_history = []
-    npc_history = []
-    player_history = []
-    story_input = ""
-    npc_input = ""
-    player_input = ""
+    while True:
+        input_type = input("input type【/attack or /talk】:")
+        if "attack" in input_type:
+            input_type = InteractType.attack
+        elif "talk" in input_type:
+            input_type = InteractType.talk
 
-    brief_task = input("请输入任务简述:")
-    if brief_task == "":
-        brief_task = "Ewan和他的伙伴帕斯卡开启了一段神奇而有趣的冒险之旅。"
-    print(brief_task)
+        user_set_input = input(f"{player.name}:")
+        # Player round
+        for agent in player.interacting_agents:
+            if isinstance(agent, ActorAgent):
+                npc_response = agent.call(player.name, agent.current_state, input_type, user_set_input)
+    
 
-    task_response = task.agent.invoke({"brief_task":brief_task 
-                                       , "input": "输出一个任务。", "chat_history":[]})
-    print("[任务系统]" + task_response['output'] + "\n<!------------------------------------------>")
-    # NPC收到任务作出反应
-    npc_response = npc.agent.invoke({"input": task_response['output']
-                                     , "chat_history": npc_history})
-    npc_response_json = parse_json(npc_response['output'])
-    npc_history.extend([HumanMessage(content=task_response['output']),AIMessage(content=npc_response['output'])])
-    print(f"[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})" + "\n<!------------------------------------------>")    
-    # player_history.append(HumanMessage(content=npc_response['output']))
+    # brief_task = input("请输入任务简述:")
+    # if brief_task == "":
+    #     brief_task = "Ewan和他的伙伴帕斯卡开启了一段神奇而有趣的冒险之旅。"
+    # print(brief_task)
 
-    story_input = f"[任务系统]{task_response['output']}\n[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})"
-    story_response = story.agent.invoke({"input": story_input, "chat_history": story_history})
-    story_history.extend([HumanMessage(content=story_input),AIMessage(content=story_response['output'])])
-    print(f"[故事]{story_response['output']}" + "\n<!------------------------------------------>")
+    # task_response = task.agent.invoke({"brief_task":brief_task 
+    #                                    , "input": "输出一个任务。", "chat_history":[]})
+    # print("[任务系统]" + task_response['output'] + "\n<!------------------------------------------>")
+    # # NPC收到任务作出反应
+    # npc_response = npc.agent.invoke({"input": task_response['output']
+    #                                  , "chat_history": npc_history})
+    # npc_response_json = parse_json(npc_response['output'])
+    # npc_history.extend([HumanMessage(content=task_response['output']),AIMessage(content=npc_response['output'])])
+    # print(f"[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})" + "\n<!------------------------------------------>")    
+    # # player_history.append(HumanMessage(content=npc_response['output']))
+
+    # story_input = f"[任务系统]{task_response['output']}\n[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})"
+    # story_response = story.agent.invoke({"input": story_input, "chat_history": story_history})
+    # story_history.extend([HumanMessage(content=story_input),AIMessage(content=story_response['output'])])
+    # print(f"[故事]{story_response['output']}" + "\n<!------------------------------------------>")
 
     
-    while True:
-        print("<!---------------------")
-        user_input = input(f"[Player]:")
-        print("--------------------->")
+    # while True:
+    #     print("<!---------------------")
+    #     user_input = input(f"[Player]:")
+    #     print("--------------------->")
 
-        player_response = player.agent.invoke({"input": "[Ewan]对[帕斯卡]说:" + user_input, "chat_history": player_history})
-        player_response_json = parse_json(player_response['output'])
+    #     player_response = player.agent.invoke({"input": user_input, "chat_history": player_history})
+    #     player_response_json = parse_json(player_response['output'])
 
-        print(f"[Ewan]{player_response_json.words}\n({player_response_json.action})" + "\n<!------------------------------------------>")
+    #     print(f"[Ewan]{player_response_json.words}\n({player_response_json.action})" + "\n<!------------------------------------------>")
 
-        npc_response = npc.agent.invoke({"input":f"[Ewan]{player_response_json.words}\n({player_response_json.action})"
-                                        , "chat_history": npc_history})
-        npc_response_json = parse_json(npc_response['output'])
-        npc_history.extend([HumanMessage(content=f"[Ewan]{player_response_json.words}\n({player_response_json.action})")
-                            , AIMessage(content=npc_response['output'])])
+    #     npc_response = npc.agent.invoke({"input":f"[Ewan]{player_response_json.words}\n({player_response_json.action})"
+    #                                     , "chat_history": npc_history})
+    #     npc_response_json = parse_json(npc_response['output'])
+    #     npc_history.extend([HumanMessage(content=f"[Ewan]{player_response_json.words}\n({player_response_json.action})")
+    #                         , AIMessage(content=npc_response['output'])])
 
-        player_history.extend([AIMessage(content=player_response['output']),HumanMessage(content=npc_response['output'])])
+    #     player_history.extend([AIMessage(content=player_response['output']),HumanMessage(content=npc_response['output'])])
 
-        print(f"[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})" + "\n<!------------------------------------------>")
+    #     print(f"[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})" + "\n<!------------------------------------------>")
 
-        story_input = f"[Ewan]{player_response_json.words}\n({player_response_json.action})\n[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})"
-        story_response = story.agent.invoke({"input": story_input, "chat_history": story_history})
-        story_history.extend([HumanMessage(content=story_input),AIMessage(content=story_response['output'])])
+    #     story_input = f"[Ewan]{player_response_json.words}\n({player_response_json.action})\n[帕斯卡]{npc_response_json.words}\n({npc_response_json.action})"
+    #     story_response = story.agent.invoke({"input": story_input, "chat_history": story_history})
+    #     story_history.extend([HumanMessage(content=story_input),AIMessage(content=story_response['output'])])
 
-        print(f"[故事]{story_response['output']}" + "\n<!------------------------------------------>")
+    #     print(f"[故事]{story_response['output']}" + "\n<!------------------------------------------>")
 
 if __name__ == "__main__":
     main()
