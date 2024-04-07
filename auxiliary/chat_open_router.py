0890f8deff1f048bf37012cd3d8c15369edc406d
Ewan <qingchen.hou@chukong-inc.com>
2024-04-07 10:01:26 +0800

    add interface class to use open router api

---
+++ auxiliary/chat_open_router.py
@@ -0,0 +1,24 @@
+from langchain_community.chat_models import ChatOpenAI
+import os
+from typing import Optional
+
+class ChatOpenRouter(ChatOpenAI):
+    openai_api_base: str
+    openai_api_key: str
+    model_name: str
+
+    def __init__(self,
+                 model_name: str,
+                 openai_api_key: Optional[str] = None,
+                 openai_api_base: str = "https://openrouter.ai/api/v1",
+                 **kwargs):
+        openai_api_key = openai_api_key or os.getenv('OPENROUTER_API_KEY')
+        super().__init__(openai_api_base=openai_api_base,
+                         openai_api_key=openai_api_key,
+                         model_name=model_name, **kwargs)
+        self.bind()
+        
+    def bind(self):
+        self.bind(response_format = { "type": "json_object" })
+
+    
